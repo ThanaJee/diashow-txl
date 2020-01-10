9d737842166bc5f10f8f20e69b433718ec346742
Thanawat <Thanawat-Jeenyoo@hotmail.de>
2020-01-10 15:41:52 +0100

    improved countMoveFiles

--- move.py
+++ move.py
@@ -1,14 +1,16 @@
-import time
-import os, shutil
+import os, shutil,time
 path = "/home/Thanawat/Dokumente/Workspace/python/sdir/"
 moveto = "/home/Thanawat/Dokumente/Workspace/python/ddir/"
+count = 0
 def exceMove():
+    global count  
     files = os.listdir(path)
     files.sort()
 #Anzahl der verschobenen Dateien
-    print(len(os.listdir(moveto)))
+    print("moved: "+ str(count) + " files")
     for f in files:
         shutil.move(path+f,moveto+f)
+	count += 1
         print(f)
 while True:
     exceMove()
